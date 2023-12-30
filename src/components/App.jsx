6e94a737210955b28242bb9995a4d81c21fc1b85
Serdiuk-Maksym <serdyukmaksimaleksandrovich@gmail.com>
2023-12-30 01:51:45 +0100

    final

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,29 @@
-export const App = () => {
+import { useState } from 'react';
+import { useDispatch } from 'react-redux';
+import { addTodo } from 'store/todoSlice';
+import TodoList from './TodoList/TodoList';
+import NewTodoForm from './InputField/InputField';
+import {} from './App.css';
+
+export function App() {
+  const [text, setText] = useState('');
+  const dispatch = useDispatch();
+
+  const handleAction = () => {
+    if (text.trim().length) {
+      dispatch(addTodo({ text }));
+      setText('');
+    }
+  };
+
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
+    <div className="App">
+      <NewTodoForm
+        value={text}
+        updateText={setText}
+        handleAction={handleAction}
+      />
+      <TodoList />
     </div>
   );
-};
+}
